63b4a85f8cf026c1370814dc3823fcd30c0a909b
clinthof <73394309+clinthof@users.noreply.github.com>
2021-12-25 22:43:48 -0500

    Begin resume work; prepare for styles

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import resume from '../../assets/Felix_Clinthorne_Resume.png';
 
 const Resume: React.FC  = () => {
     return (
-        <div id="resume">
-            Resume
+        <div id="resume-page-container">
+            <div id="resume">
+                <img alt='resume' src={String(resume)} />
+            </div>
         </div>
     )
 }
--- src/data.tsx
+++ src/data.tsx
@@ -6,23 +6,23 @@ interface Params {
 
 export const links: Array<Params> = [
     {
-        id:1,
-        text:'about',
-        url:'/',
+        id: 1,
+        text: 'about',
+        url: '/',
     },
     {
-        id:2,
-        text:'projects',
-        url:'/projects',
+        id: 2,
+        text: 'projects',
+        url: '/projects',
     },
     {
-        id:3,
-        text:'resume',
-        url:'/resume',
+        id: 3,
+        text: 'resume',
+        url: '/resume',
     },
     {
-        id:4,
-        text:'contact',
-        url:'/contact',
+        id: 4,
+        text: 'contact',
+        url: '/contact',
     },
 ]
