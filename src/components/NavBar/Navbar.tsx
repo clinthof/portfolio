f3515b8211890cd36b6bc0b23c4557420f403159
clinthof <73394309+clinthof@users.noreply.github.com>
2021-12-29 18:15:08 -0500

    Reformat and prepare for any necessary framer animations

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -4,20 +4,21 @@ import { CustomNav, CustomLink, Logo, NavLinks, } from '../../Styles'
 
 const Navbar: React.FC = () => {
     return (
-        <>
-            <CustomNav>
-                <Logo>
-                    <CustomLink to='/'>felix.</CustomLink>
-                </Logo>
-                <NavLinks>
-                    {links.map(link => 
-                        <CustomLink to={link.url} key={link.id}>
-                            {link.text}
-                        </CustomLink>
-                        )}
-                </NavLinks>
-            </CustomNav>
-        </>
+        <CustomNav>
+            <Logo>
+                <CustomLink to='/'>felix.</CustomLink>
+            </Logo>
+            <NavLinks>
+                {links.map(link => 
+                    <CustomLink 
+                        to={link.url}
+                        key={link.id}
+                    >
+                        {link.text}
+                    </CustomLink>
+                    )}
+            </NavLinks>
+        </CustomNav>
     )
 }
 
