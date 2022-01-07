a2e13ff7f3feb6f587dcee3c9e734e68d3ccf0a9
clinthof <73394309+clinthof@users.noreply.github.com>
2022-01-07 15:50:39 -0500

    Create custom footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterWrapper } from '../../Styles';
+
+const Footer: React.FC = () => {
+    return (
+        <FooterWrapper>
+            Footer component
+        </FooterWrapper>
+    )
+}
+
+export default Footer;
