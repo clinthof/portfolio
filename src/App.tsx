04f995734a0cd3694728a5569f945eab834db753
clinthof <73394309+clinthof@users.noreply.github.com>
2022-01-09 00:44:03 -0500

    Try out new react-router for smooth scroll; back to wouter for animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from "wouter";
+import { Route, Switch } from 'wouter';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Resume from './components/Resume/Resume';
 import Contact from './components/Contact/Contact';
 import Navbar from './components/NavBar/Navbar';
-import Home from './components/Home/Home';
-import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <Switch>
-        <Route path='/' component={Home} />
+        <Route path='/' component={About} />
         <Route path='/about' component={About} />
         <Route path='/projects' component={Projects} />
         <Route path='/resume' component={Resume} />
         <Route path='/contact' component={Contact} />
       </Switch>
-      <Footer />
     </>
   );
 }
