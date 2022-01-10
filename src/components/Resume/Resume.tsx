1276e0157f0487feaacbd9e41c0a0b22e4c36015
clinthof <73394309+clinthof@users.noreply.github.com>
2022-01-10 15:38:14 -0500

    Remove download button

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import resume from '../../assets/Felix_Clinthorne_Resume.png';
-import DownloadIcon from '@mui/icons-material/Download';
-import { Button } from '../../Styles';
 
 const Resume: React.FC = () => {
     return (
@@ -21,19 +19,6 @@ const Resume: React.FC = () => {
                         'width':'60%',
                     }}
                 />
-            </div>
-            <div id='download-btn-wrapper'
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    margin: '2%',
-                }}>
-                <a href="path_to_file">
-                    <Button>
-                        <DownloadIcon />
-                        download
-                    </Button>
-                </a>
             </div>
         </div>
 
