f69154bddfa0ac10eb16728b2f245ddda9399088
clinthof <73394309+clinthof@users.noreply.github.com>
2022-01-13 12:10:26 -0500

    Add skillcard data

--- src/data.tsx
+++ src/data.tsx
@@ -4,8 +4,8 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import { ReactElement } from 'react';
 
 interface ProfileData {
-    href: string,
     id: string,
+    href: string,
     component: ReactElement
 }
 
@@ -24,8 +24,8 @@ interface ProjectData {
 
 const profileLinks: Array<ProfileData> = [
     {
-        href: 'https://github.com/clinthof',
         id: 'github-profile',
+        href: 'https://github.com/clinthof',
         component: <GitHubIcon className='social-icon' />,
     },
     {
@@ -82,42 +82,120 @@ const projects: Array<ProjectData> = [
         tools: 'Swift, Objective-C',
         description: 'descriptive description',
     },
-    {
-        id: 4,
-        title: 'GCD Map',
-        tools: 'React, python, AWS',
-        description: 'description here',
-    },
-    {
-        id: 5,
-        title: 'COVID-19 Data Dashboard',
-        tools: 'Vue, Firebase',
-        description: 'description here',
-    },
-    {
-        id: 6,
-        title: 'ArtPrize Mobile Application',
-        tools: 'Swift, Objective-C',
-        description: 'descriptive description',
-    },
-    {
-        id: 7,
-        title: 'GCD Map',
-        tools: 'React, python, AWS',
-        description: 'description here',
-    },
-    {
-        id: 8,
-        title: 'COVID-19 Data Dashboard',
-        tools: 'Vue, Firebase',
-        description: 'description here',
-    },
-    {
-        id: 9,
-        title: 'ArtPrize Mobile Application',
-        tools: 'Swift, Objective-C',
-        description: 'descriptive description',
-    },
 ];
 
-export {navLinks, projects, profileLinks};
+const skills = [
+    {
+        skillName: 'Typescript',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                alt='typescript'
+                className='skill-img' 
+            />
+    },
+    {
+        skillName: 'Python',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
+                alt='python'
+                className='skill-img' 
+                />,
+    },
+    {
+        skillName: 'Java',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" 
+                alt='java'
+                className='skill-img' 
+            />
+        ,
+    },
+    {
+        skillName: 'Swift',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg" 
+                alt='swift'
+                className='skill-img' 
+            />,
+    },
+    {
+        skillName: 'Kotlin',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-plain-wordmark.svg"
+                alt='kotlin'
+            />,
+    },
+    {
+        skillName: 'C',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg"
+                alt='c-lang'
+                className='skill-img' 
+                />,
+    },
+    {
+        skillName: 'React',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+                alt='react'
+                className='skill-img' 
+            />,
+    },
+    {
+        skillName: 'Vue',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg"
+                alt='vue'
+                className='skill-img'
+            />,
+    },
+    {
+        skillName: 'Firebase',
+        img: <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg"
+                alt='firebase'
+                className='skill-img' 
+            />
+    },
+    {
+        skillName: 'MySQL',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
+                alt='mysql'
+                className='skill-img'
+                />
+    },
+    {
+        skillName: 'AWS',
+        img: <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg"
+                alt='aws'
+                className='skill-img' 
+                />,
+    },
+    {
+        skillName: 'Git',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
+                alt='git'
+                className='skill-img'
+            />,
+    },
+    {
+        skillName: 'Google Cloud',
+        img: <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original-wordmark.svg"
+                alt='gcloud'
+                className='skill-img'
+            />,
+    },
+    {
+        skillName: 'Docker',
+        img: <img 
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg"
+                alt='docker'    
+            />
+    }
+]
+
+export {navLinks, projects, profileLinks, skills};
