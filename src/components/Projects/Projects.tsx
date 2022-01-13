ff2af2c3d38821f3b937f4f58293d0c9cbe270f7
clinthof <73394309+clinthof@users.noreply.github.com>
2022-01-13 12:11:12 -0500

    Add skills and project card template

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { projects } from '../../data';
-import { CardWrapper } from '../../Styles';
+import { projects, skills } from '../../data';
+import { motion } from 'framer-motion';
+import { cardWrapVariant, cardVariant } from '../../variants';
 
 const Projects: React.FC = () => {
     return (
@@ -8,13 +8,43 @@ const Projects: React.FC = () => {
             className='content-container'
             id='projects-container'
         >
-            <h1>Recent Projects</h1>
-            <CardWrapper>
+            <h1>Some things I've worked on lately</h1>
+            <motion.div
+                className='project-page-container'
+                id='card-container'
+                variants={cardWrapVariant}
+                initial='hidden'
+                animate='visible'
+            >
                 {projects.map(project => 
-                    <div key={project.id}>
-                        {project.title} - {project.tools}
-                    </div>)}
-            </CardWrapper>
+                    <motion.div 
+                        key={project.id}
+                        variants={cardVariant}
+                        className='project-card'
+                        style={{'border': '2px solid black'}}
+                    >
+                        Project
+                    </motion.div>)}
+            </motion.div>
+            <h1 style={{'marginTop': '5%'}}>
+                Some tools I've explored
+            </h1>
+            <motion.div 
+                className='project-page-container'
+                id='skill-container'
+                variants={cardWrapVariant}
+                initial='hidden'
+                animate='visible'
+            >
+                {skills.map(skill => 
+                    <motion.div 
+                        key={skill.skillName}
+                        variants={cardVariant}
+                        className='skill-tile'
+                    >
+                        {skill.img}
+                    </motion.div>)} 
+            </motion.div>
         </div>
     )
 };
