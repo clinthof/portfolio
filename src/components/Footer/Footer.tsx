8d27a73188700230403f059b73f44d9786ce61f1
clinthof <73394309+clinthof@users.noreply.github.com>
2022-01-18 13:04:56 -0500

    Remove footer styled component(s) and file

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { FooterContainer } from './FooterStyles';
 
 const Footer: React.FC = () => {
     return (
-        <FooterContainer>
-            <h3 
-                style={{ 
-                    color: "white", 
-                    textAlign: "center", 
-                    marginTop: "-60px" 
-                }}
-            >
-        Check me out elsewhere:
-      </h3>
-        </FooterContainer>
+    //     <FooterContainer>
+    //         <h3 
+    //             style={{ 
+    //                 color: "white", 
+    //                 textAlign: "center", 
+    //                 marginTop: "-60px" 
+    //             }}
+    //         >
+    //     Check me out elsewhere:
+    //   </h3>
+    //     </FooterContainer>
+    <></>
     )
 }
 
