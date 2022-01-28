7268eed2c40c21871e5bbad6949c9ed8e62f1281
clinthof <73394309+clinthof@users.noreply.github.com>
2022-01-28 12:39:49 -0500

    Add new color to global style

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const GlobalStyle = createGlobalStyle`
   html {
     font-family: 'Lucida Sans', 'Lucida Sans Regular', 
     'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-    background-color: #17191a;
+    background-color: #191a1b;
   }
 `;
 
