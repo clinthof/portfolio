33470e19435539cf560d44dca98f4ef6f712d37d
clinthof <73394309+clinthof@users.noreply.github.com>
2022-01-28 14:07:47 -0500

    Set up wouter route hook; slightly restructure generated links

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link, useRoute } from 'wouter';
 import { navLinks } from '../../data';
 import { CustomNav, CustomLink, Logo, NavLinks, } from '../../Styles';
 
 const Navbar: React.FC = () => {
+    const [isActive] = useRoute('');
+
     return (
         <CustomNav>
             <Logo>
@@ -10,9 +13,11 @@ const Navbar: React.FC = () => {
             </Logo>
             <NavLinks>
                 {navLinks.map(link => 
-                        <CustomLink to={link.url} key={link.id}>
-                            {link.text}
-                        </CustomLink>
+                        <Link to={link.url} key={link.id}>
+                            <CustomLink className={isActive ? 'active' : ''}>
+                                {link.text}
+                            </CustomLink>
+                        </Link>
                     )}
             </NavLinks>
         </CustomNav>
