8f799fa53c7e06a0ce4315e62c0d2480fc316049
clinthof <73394309+clinthof@users.noreply.github.com>
2022-02-03 11:52:33 -0500

    Title changes and reflection of changes to card component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,18 +1,18 @@
 import './Projects.css';
-import { skills } from '../../data';
-import ProjectCards from './ProjectCards';
+import { projects, skills } from '../../data';
+import ProjectCard from './ProjectCard';
 
 const Projects: React.FC = () => {
     return (
         <div className='content-container'>
             <h1 className='project-page-title'>
-                Some things I've worked on lately
+                Some recent projects
             </h1>
             <div className='project-page-container' id='card-container'>
-                <ProjectCards />
+                {projects.map(project => <ProjectCard project={project} />)}
             </div>
             <h1 className='project-page-title'>
-                Some tools I've explored
+                Toolbox
             </h1>
             <div className='project-page-container' id='skill-container'>
                 {skills.map(skill => 
